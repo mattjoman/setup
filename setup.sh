6a7ed1cd69b84f7b6959af86fb0eb50c2032642c
Your Name <you@example.com>
2023-01-31 20:04:56 +0000

    added yt-dlp

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ if [ "$input" = 'y' ]; then
   sudo pacman -S xorg xorg-xinit xorg-backlight xf86-video-intel xorg-xfontsel xwallpaper picom                # xorg stuff
   sudo pacman -S qtile                                                                                         # qtile
   sudo pacman -S zathura zathura-pdf-poppler dmenu neovim alacritty python python-pip sxiv thunderbird cups    # useful
-  sudo pacman -S vlc                                                                                           # media
+  sudo pacman -S vlc yt-dlp                                                                                    # media
   sudo pacman -S thunar ranger flameshot                                                                       # useful
   sudo pacman -S firefox-developer-edition                                                                     # browser
   sudo pacman -S alsa-utils pulseaudio pulseaudio-alsa pavucontrol pulsemixer                                  # sound
